597cdbd67a5542de317e4311a5196bd3dd9eff17
Muhammad Irfan <irfan@jojonomic.com>
2022-10-03 22:54:04 +0700

    [Tugas 3] simple operation

---
+++ main.py
@@ -0,0 +1,18 @@
+num1 = int(input("Pick a Number: "))
+num2 = int(input("Pick Another Number: "))
+
+SUM = num1 + num2
+print("Number A + Number B = ")
+print(SUM)
+
+num3 = num1 - num2
+print("Number A - Number B = ")
+print(num3)
+
+num4 = num1 * num2
+print("Number A * Number B = ")
+print(num4)
+
+num5 = num1 / num2
+print("Number A / Number B = ")
+print(num5)
